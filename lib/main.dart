31f137358c2aa2fd75eaf391cfa9757415b31311
FlutLab.io <>
2022-06-01 07:47:09 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:talamantes/models/catalog.dart';
+import 'package:talamantes/widgets/itemWidget.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Celulares Cunumi el faraon"),
+        ),
+        body: ListView.builder(
+            itemCount: CatalogModel.items.length,
+            itemBuilder: (context, index) {
+              return ItemWidget(item: CatalogModel.items[index]);
+            }),
+      ),
+    );
+  }
+}
